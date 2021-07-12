767609669140a9ef6114d64e675a955dac0455bf
Łukasz Niewinski <gdx137674@student.wsb.gda.pl>
2021-07-12 20:24:20 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace WazneKody2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label operations;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.search_Lebel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.search_Result = new System.Windows.Forms.Label();
+            this.Pole_Kodu = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.licznik_VjuS = new System.Windows.Forms.Label();
+            operations = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // search_Lebel
+            // 
+            this.search_Lebel.AutoSize = true;
+            this.search_Lebel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.search_Lebel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(178)))));
+            this.search_Lebel.Location = new System.Drawing.Point(92, 28);
+            this.search_Lebel.Name = "search_Lebel";
+            this.search_Lebel.Size = new System.Drawing.Size(180, 20);
+            this.search_Lebel.TabIndex = 0;
+            this.search_Lebel.Text = "Wpisz czego szukasz";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(115, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // operations
+            // 
+            operations.AutoSize = true;
+            operations.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            operations.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(178)))));
+            operations.Location = new System.Drawing.Point(103, 83);
+            operations.Name = "operations";
+            operations.Size = new System.Drawing.Size(161, 20);
+            operations.TabIndex = 2;
+            operations.Tag = "";
+            operations.Text = "Dostępne operacje";
+            operations.Visible = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(54)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(146)))), ((int)(((byte)(249)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(70, 109);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(234, 64);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.Visible = false;
+            // 
+            // search_Result
+            // 
+            this.search_Result.AutoSize = true;
+            this.search_Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.search_Result.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(178)))));
+            this.search_Result.Location = new System.Drawing.Point(112, 178);
+            this.search_Result.Name = "search_Result";
+            this.search_Result.Size = new System.Drawing.Size(146, 18);
+            this.search_Result.TabIndex = 4;
+            this.search_Result.Text = "Wynik wyszukania";
+            this.search_Result.Visible = false;
+            // 
+            // Pole_Kodu
+            // 
+            this.Pole_Kodu.AutoSize = true;
+            this.Pole_Kodu.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Pole_Kodu.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(146)))), ((int)(((byte)(249)))));
+            this.Pole_Kodu.Location = new System.Drawing.Point(96, 201);
+            this.Pole_Kodu.Name = "Pole_Kodu";
+            this.Pole_Kodu.Size = new System.Drawing.Size(104, 16);
+            this.Pole_Kodu.TabIndex = 5;
+            this.Pole_Kodu.Text = "Kod Aktywacji";
+            this.Pole_Kodu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.toolTip1.SetToolTip(this.Pole_Kodu, "Kliknij 2 razy LPP aby skopiować!");
+            this.Pole_Kodu.Visible = false;
+            // 
+            // licznik_VjuS
+            // 
+            this.licznik_VjuS.AutoSize = true;
+            this.licznik_VjuS.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.licznik_VjuS.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(158)))), ((int)(((byte)(161)))), ((int)(((byte)(178)))));
+            this.licznik_VjuS.Location = new System.Drawing.Point(194, 280);
+            this.licznik_VjuS.Name = "licznik_VjuS";
+            this.licznik_VjuS.Size = new System.Drawing.Size(45, 16);
+            this.licznik_VjuS.TabIndex = 6;
+            this.licznik_VjuS.Text = "label4";
+            this.licznik_VjuS.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
+            this.ClientSize = new System.Drawing.Size(373, 301);
+            this.Controls.Add(this.licznik_VjuS);
+            this.Controls.Add(this.Pole_Kodu);
+            this.Controls.Add(this.search_Result);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(operations);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.search_Lebel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ważne Kody";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label search_Lebel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label search_Result;
+        private System.Windows.Forms.Label Pole_Kodu;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label licznik_VjuS;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WazneKody2
+{
+
+
+
+    public partial class Form1 : Form
+    {
+        public class Infos
+        {
+            private string info1;
+            private string info2;
+
+
+            public Infos(string s1, string s2)
+            {
+                info1 = s1;
+                info2 = s2;
+            }
+            public string Info1
+            {
+                get { return this.info1; }
+                set { this.info1 = value; }
+            }
+
+            public string Info2
+            {
+                get { return this.info2; }
+                set { this.info2 = value; }
+            }
+
+            public override string ToString()
+            {
+                return this.info2;
+            }
+        }
+
+        List<Infos> test = new List<Infos>();
+        Infos inf1 = new Infos("solo s", "Kod aktywacji: *111*572*1#" + Environment.NewLine + "Czas trwania: 30 dni" + Environment.NewLine + "Cena: 20zł " + Environment.NewLine + "Pakiet odnowi się za 30 dni");
+        Infos inf2 = new Infos("solo m", "Kod aktywacji: *111*978*1#" + Environment.NewLine + "Czas trwania: 30 dni" + Environment.NewLine + "Cena: 25zł " + Environment.NewLine + "Pakiet odnowi się za 30 dni");
+        Infos inf3 = new Infos("solo l", "Kod aktywacji: *111*571*1#" + Environment.NewLine + "Czas trwania: 30 dni" + Environment.NewLine + "Cena: 30zł " + Environment.NewLine + "Pakiet odnowi się za 30 dni");
+        Infos inf4 = new Infos("solo xl", "Kod aktywacji: *111*203*1#" + Environment.NewLine + "Czas trwania: 30 dni" + Environment.NewLine + "Cena: 35zł " + Environment.NewLine + "Pakiet odnowi się za 30 dni");
+        Infos inf5 = new Infos("solo xxl", "Kod aktywacji: *111*223*1#" + Environment.NewLine + "Czas trwania: 30 dni" + Environment.NewLine + "Cena: 40zł " + Environment.NewLine + "Pakiet odnowi się za 30 dni");
+        Infos inf6 = new Infos("play na karte", "Kod aktywacji: *163*6#");
+        Infos inf7 = new Infos("formuła play na karte", "Kod aktywacji: *163*10#");
+        Infos inf8 = new Infos("play na karte lubię to", "Kod aktywacji: *163*7#");
+        Infos inf9 = new Infos("sprawdzenie aktualnej taryfy", "Kod aktywacji: *111*49*1#");
+        Infos inf10 = new Infos("play na karte odnowa", "Kod aktywacji: *163*20#");
+        Infos inf11 = new Infos("internet na karte 2.0", "Kod aktywacji: *163*21#");
+        Infos inf12 = new Infos("rbm siema na kartę", "Kod aktywacji: *163*21#");
+        Infos inf13 = new Infos("rbm siema na kartę pl", "Kod aktywacji: *163*21#");
+        Infos inf14 = new Infos("stan nielimitowany i 10 GB w RBM", "Kod aktywacji: *111*964*1#" + Environment.NewLine + "Czas trwania 30 dni" + Environment.NewLine + "Cena: 30zł " + Environment.NewLine + "Pakiet odnowi się za 30 dni");
+        Infos inf15 = new Infos("1000 minut UA + 15GB DATA", "Kod aktywacji cykliczny: *111*1198*1#" + Environment.NewLine + "Kod aktywacji 1-razowy: *111*1197*1#" + Environment.NewLine + "Czas trwania: 30 dni" + Environment.NewLine + "Cena: 35zł");
+        Infos inf16 = new Infos("1000 minut UA + 15GB", "Kod aktywacji cykliczny: *111*2000*1#" + Environment.NewLine + "Kod aktywacji 1-razowy: *111*1199*1#" + Environment.NewLine + "Czas trwania: 30 dni" + Environment.NewLine + "Cena: 35zł");
+        Infos inf17 = new Infos("1000 minut UA + 10GB viber", "Kod aktywacji 1-razowy: *111*331*1#" + Environment.NewLine + "Czas trwania: 30 dni" + Environment.NewLine + "Cena: 30zł");
+        Infos inf18 = new Infos("1000 minut UA", "Kod aktywacji 1-razowy: *111*969*1#" + Environment.NewLine + "Czas trwania: 30 dni" + Environment.NewLine + "Cena: 25ł");
+        Infos inf19 = new Infos("tańsze połączenia ua", "Kod aktywacji: *111*201*1#" + Environment.NewLine + "Cena: 0zł");
+        Infos inf20 = new Infos("dzien bez limtu GB DATA", "Kod aktywacji 1-razowy: *111*1191*1#" + Environment.NewLine + "Czas trwania 1 dzień" + Environment.NewLine + "Cena: 5zł");
+        Infos inf21 = new Infos("tydzien bez limtu GB DATA", "Kod aktywacji 1-razowy: *111*1192*1#" + Environment.NewLine + "Czas trwania 7 dni" + Environment.NewLine + "Cena: 20zł");
+        Infos inf22 = new Infos("miesiąc bez limtu GB DATA", "Kod aktywacji cykliczny:  *111*1194*1#" + Environment.NewLine + "Czas trwania 30 dni" + Environment.NewLine + "Cena: 50zł" + Environment.NewLine + "Pakiet odnowi się za 30 dni");
+        Infos inf23 = new Infos("stan gb viber", "Kod aktywacji: *633#");
+        Infos inf24 = new Infos("stan darmowy", "Kod aktywacji: *188#");
+        Infos inf25 = new Infos("1000 minut UA RBM", "Kod aktywacji 1-razowy: *111*981*1#" + Environment.NewLine + "Czas trwania 30 dni" + Environment.NewLine + "Cena: 25zł");
+        Infos inf26 = new Infos("stan konta pre-paid", "Kod aktywacji: *101#");
+        Infos inf27 = new Infos("stan gb w MIX", "Kod aktywacji: *102#");
+        Infos inf28 = new Infos("stan minut do wszystkich", "Kod aktywacji: *104#");
+        Infos inf29 = new Infos("stan gb w postpaid", "Kod aktywacji: *107#");
+        Infos inf30 = new Infos("stan gb w prepaid", "Kod aktywacji: *108#");
+        Infos inf31 = new Infos("zarządzanie kontem", "Kod aktywacji: *111#");
+        Infos inf32 = new Infos("sprawdź swoj numer", "Kod aktywacji: *121#");
+        Infos inf33 = new Infos("prośba o doładowanie", "Kod aktywacji: *122#");
+        Infos inf34 = new Infos("info. o płatnościach", "Kod aktywacji: *125#");
+        Infos inf35 = new Infos("muzka na czekanie", "Kod aktywacji: *123#");
+        Infos inf36 = new Infos("strefa stacjonarna", "Kod aktywacji: *313#" + Environment.NewLine + "Kod aktywacji: 790 313 313");
+        Infos inf37 = new Infos("stan gb w UE", "Kod aktywacji: *575#" + Environment.NewLine + "Kod aktywacji: *800#");
+        Infos inf38 = new Infos("stan doładowań mix", "Kod aktywacji: *111*357*5#");
+        Infos inf39 = new Infos("poczta głosowa", "Kod aktywacji: *111*56*1#" + Environment.NewLine + "Numer poczty:790 200 200" + Environment.NewLine + "Numer pro: 790 200 300");
+        Infos inf40 = new Infos("wydłużenie kontraktu mix", "Kod aktywacji: *111*888*1#");
+        Infos inf41 = new Infos("autokonfiguracja", "Kod aktywacji: sms o treści PLAY na 9999" + Environment.NewLine + "Strona: https://konfiguracja.play.pl/");
+        Infos inf42 = new Infos("stan 100GB na lato", "Kod aktywacji: *107#" + Environment.NewLine + "Dla 'play na karte lubie to' użyj: *108#");
+        Infos inf43 = new Infos("stan 1000 minut ua", "Kod aktywacji: *111*996#");
+        Infos inf44 = new Infos("pewny telefon", "Numer: 48 791 100 310");
+        Infos inf45 = new Infos("pakiet medyczny", "Numer: 22 529 17 62");
+        Infos inf46 = new Infos("play360", "Numer: 732 666 360" + Environment.NewLine + "Strona: https://www.naprawaplay.pl/");
+        Infos inf47 = new Infos("bezpieczny telefon", "Numer: 791 100 310");
+        Infos inf48 = new Infos("ochrona wyswietlacza", "Numer: 799 599 900");
+        Infos inf49 = new Infos("plan na zdrowie prudential", "Numer: 801 386 000");
+        Infos inf50 = new Infos("sony", "Numer: 801 382 462");
+        Infos inf51 = new Infos("alcatel", "22 58 30 081");
+        Infos inf52 = new Infos("lenovo", "Numer: 22 273 97 77");
+        Infos inf53 = new Infos("garmin", "Numer: 800 441 24 54 ");
+        Infos inf54 = new Infos("samsung", "Numer: 801 672 678");
+        Infos inf55 = new Infos("lg", "Numer: 801 545 454");
+        Infos inf56 = new Infos("htc", "Numer: 800 702 680");
+        Infos inf57 = new Infos("motorola", "Numer: 22 307 03 60");
+        Infos inf58 = new Infos("myphone", "Numer: 71 717 74 00");
+        Infos inf59 = new Infos("huawei", "Numer: 800 811 810");
+        Infos inf60 = new Infos("zte", "Numer: 22 726 38 10");
+        Infos inf61 = new Infos("acer", "Numer: 22 471 07 77");
+        Infos inf62 = new Infos("kiano", "Numer: 12 383 31 30");
+        Infos inf63 = new Infos("hp", "Numer: 22 583 43 71");
+        Infos inf64 = new Infos("prestigio", "Numer: 22 337 15 48");
+        Infos inf65 = new Infos("asus", "Numer: 22 571 80 40");
+
+
+
+        private void SetupList()
+        {
+            test.Add(inf1);
+            test.Add(inf2);
+            test.Add(inf3);
+            test.Add(inf4);
+            test.Add(inf5);
+            test.Add(inf6);
+            test.Add(inf7);
+            test.Add(inf8);
+            test.Add(inf9);
+            test.Add(inf42);
+            test.Add(inf10);
+            test.Add(inf11);
+            test.Add(inf12);
+            test.Add(inf13);
+            test.Add(inf14);
+            test.Add(inf15);
+            test.Add(inf16);
+            test.Add(inf17);
+            test.Add(inf18);
+            test.Add(inf19);
+            test.Add(inf20);
+            test.Add(inf21);
+            test.Add(inf22);
+            test.Add(inf23);
+            test.Add(inf24);
+            test.Add(inf25);
+            test.Add(inf26);
+            test.Add(inf27);
+            test.Add(inf28);
+            test.Add(inf29);
+            test.Add(inf30);
+            test.Add(inf31);
+            test.Add(inf32);
+            test.Add(inf33);
+            test.Add(inf34);
+            test.Add(inf35);
+            test.Add(inf36);
+            test.Add(inf37);
+            test.Add(inf38);
+            test.Add(inf39);
+            test.Add(inf40);
+            test.Add(inf41);
+            test.Add(inf42);
+            test.Add(inf43);
+            test.Add(inf44);
+            test.Add(inf45);
+            test.Add(inf46);
+            test.Add(inf47);
+            test.Add(inf48);
+            test.Add(inf49);
+            test.Add(inf50);
+            test.Add(inf51);
+            test.Add(inf52);
+            test.Add(inf53);
+            test.Add(inf54);
+            test.Add(inf55);
+            test.Add(inf56);
+            test.Add(inf57);
+            test.Add(inf58);
+            test.Add(inf59);
+            test.Add(inf60);
+            test.Add(inf61);
+            test.Add(inf62);
+            test.Add(inf63);
+            test.Add(inf64);
+            test.Add(inf65);
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            listcollection.Clear();
+            SetupList();
+
+
+
+
+            foreach (var p in test)
+            {
+                listBox1.Items.Add(p.Info1);
+            }
+
+            foreach (string str in listBox1.Items)
+            {
+                listcollection.Add(str);
+                textBox1.CharacterCasing = CharacterCasing.Lower;
+            }
+        }
+        List<string> listcollection = new List<string>();
+
+        int licznik = 0;
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+
+            var curItem = test.FirstOrDefault(x => x.Info1 == listBox1.SelectedItem.ToString());
+            licznik = licznik + 1;
+            licznik_VjuS.Text = "Aplikacja pomogła Ci " + licznik.ToString() + " razy";
+            licznik_VjuS.Visible = true;
+            Pole_Kodu.Visible = true;
+            Pole_Kodu.Text = (curItem.ToString());
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text) == false)
+            {
+                listBox1.Visible = true;
+                //operations.Visible = true;
+                search_Result.Visible = true;
+                listBox1.Items.Clear();
+                foreach (string str in listcollection)
+                {
+                    if (str.StartsWith(textBox1.Text))
+                    {
+                        listBox1.Items.Add(str);
+                    }
+                }
+            }
+            else if (textBox1.Text == "")
+            {
+                Pole_Kodu.Visible = false;
+                //operations.Visible = false;
+                search_Result.Visible = false;
+                listBox1.Visible = false;
+                listBox1.Items.Clear();
+                foreach (string str in listcollection)
+                {
+                    listBox1.Items.Add(str);
+                }
+            }
+        }
+
+
+    }
+}
